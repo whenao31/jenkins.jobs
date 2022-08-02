73949bfb98ecb6310915c09e84c96e75a324a538
Wilton Henao <90293972+whenao31@users.noreply.github.com>
2022-08-02 17:09:14 -0500

    job in a sh file

---
+++ jobscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Starting the loop
+for a in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+do
+    #When the loop gets 8, hang on 5 secs
+    if [ $a == 8 ]
+    then
+    	sleep 5
+        echo "Get some rest from the class, $name"
+    fi
+    echo "Class number $a"
+done
+sleep 5
+echo "$name, we are done with $curso course, taught in $language, See you soon!"
