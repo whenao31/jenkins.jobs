db779c2886eee9b4241b7cc065d42c4a49fd0e85
Wilton Henao <90293972+whenao31@users.noreply.github.com>
2022-08-03 09:38:23 -0500

    Create parametreizedDSL.groovy

---
+++ parameterizedDSL.groovy
@@ -0,0 +1,20 @@
+job('ejemplo-job-DSL'){
+  description('Job DSL de ejemplo para el curso')
+  scm {
+    git('https://github.com/whenao31/jenkins.jobs.git', 'main') { node -> 
+      node / gitConfigName('whenao31')
+      node / gitConfigEmail('whenao86@gmail.com')
+    } 
+  }
+  parameters {
+    stringParam('name', defaultValue = 'Wil', description = 'String param for the job')
+    choiceParam('curso', ['jenkins', 'kubernetes', 'aws'])
+    choiceParam('language', ['spanish', 'english', 'chinese', 'italian', 'french'])
+  }
+  /*triggers {
+    cron('H/7 * * * *')
+  }*/
+  steps {
+    shell("bash jobscript.sh")
+  }
+}
